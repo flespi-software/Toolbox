0f35ab49de96e8d03dacbe465b62ec7aa3977bc8
Siarhei Buntsevich <sebu@gurtam.com>
2019-05-27 11:18:21 +0300

    publish: v.0.15.8
    generated from commit 347d87eb62c9e1aca03c02b0cb1f3abd419e21b0

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.b7f42ea3beb0ce8fc8719deb7742ed06.js"
+  "precache-manifest.3638e8843a37bff9921828baaaaeef35.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "toolbox"});
